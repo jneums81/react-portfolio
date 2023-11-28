1d6b3a3e007c29923a7fee4f6767d1c8de1f3063
Jackson Neuman <jacksonneuman@Jacksons-Air-4.attlocal.net>
2023-11-27 18:03:47 -0600

    Revised about me page and and added some css

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <h1>Your Name</h1>
+      <h1>Jackson Neuman</h1>
       <Navigation />
     </header>
   );
--- src/sections/AboutMe.js
+++ src/sections/AboutMe.js
@@ -1,12 +1,21 @@
 // src/sections/AboutMe.js
 import React from 'react';
+import './AboutMe.css';
 
 const AboutMe = () => {
   return (
     <section className="about-me">
       {/* Content for the About Me section goes here */}
       <h2>About Me</h2>
-      <p>Your short bio and photo/avatar here.</p>
+      <p>Hello, I'm Jackson Neuman, a passionate web developer based in Naperville, Illinois. With a love for coding and a knack for problem-solving, I thrive on turning ideas into innovative and user-friendly digital experiences.
+
+I specialize in front-end technologies, including HTML, CSS, and JavaScript, with expertise in building responsive and dynamic web applications. Whether it's creating a seamless user interface or diving into the intricacies of web development frameworks, I'm always excited to take on new challenges.
+
+My journey in the world of web development began with a curiosity to understand how things work on the internet. Since then, I've had the pleasure of working on diverse projects that range from personal websites to collaborative applications.
+
+When I'm not coding, you can find me exploring the latest trends in tech, experimenting with new coding techniques, or enjoying a good cup of coffee while brainstorming my next project.
+
+Let's connect! I'm always open to collaborations, discussions about technology, or just a friendly chat. Feel free to reach out through the Contact page—I'd love to hear from you!</p>
     </section>
   );
 };
